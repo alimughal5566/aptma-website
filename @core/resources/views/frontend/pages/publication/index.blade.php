1a83b5a080d04d815c88129c523c81cecbf3a556
Muazzam Younus <muazzam.younus925@gmail.com>
2021-02-12 03:07:04 +0500

    Updates Books, Publication, Circular Page Design

--- @core/resources/views/frontend/pages/publication/index.blade.php
+++ @core/resources/views/frontend/pages/publication/index.blade.php
@@ -6,33 +6,42 @@
     Publications
 @endsection
 @section('page-meta-data')
-    <meta name="description" content="{{get_static_option('service_page_'.$user_select_lang_slug.'_meta_description')}}">
+    <meta name="description"
+          content="{{get_static_option('service_page_'.$user_select_lang_slug.'_meta_description')}}">
     <meta name="tags" content="{{get_static_option('service_page_'.$user_select_lang_slug.'_meta_tags')}}">
 @endsection
 @section('content')
-    <section class="service-area service-page padding-120">
+    <section class="service-area service-page common-area publication-area publication-page padding-top-40 padding-bottom-60">
         <div class="container">
             <div class="row">
                 @php $a = 1; @endphp
-                @foreach($all_services as$data)
+                @foreach($all_services as $data)
                     <div class="col-lg-4 col-md-6">
-                        <div class="single-what-we-cover-item-02 margin-bottom-30">
-                            <div class="single-what-img "  style="max-height: 50%;">
-                                {!! render_image_markup_by_attachment_id($data->thumbnail) !!}
-                            </div>
-                            <div class="content">
-                                <a href="{{route('frontend.publication.single',$data->id)}}"><h4 class="title">{{$data->title}}</h4></a>
-                                <p>Published Date:{{\Carbon\Carbon::parse($data->publish_date)->format('d M Y')}}</p>
-
+                        <div class="common-item publication-item single-what-we-cover-item-02 margin-bottom-30">
+                            <div class="common-img publication-img single-what-img position-relative">
                                 @php
                                     $now = Carbon\Carbon::now();
-                                $datework = Carbon\Carbon::parse($data->created_at);
-                                $diff = $datework->diffInDays($now); @endphp
+                                    $datework = Carbon\Carbon::parse($data->created_at);
+                                    $diff = $datework->diffInDays($now);
+                                @endphp
                                 @if($diff<15)
-                                    <small class="float-right text-danger font-italic badge badge-warning" >New</small>
+                                    <small class="font-italic badge ">New</small>
                                 @endif
+                                <a href="{{route('frontend.publication.single',$data->id)}}">
+                                    {!! render_image_markup_by_attachment_id($data->thumbnail) !!}
+                                </a>
+                            </div>
+                            <div class="common-content content">
+                                <a href="{{route('frontend.publication.single',$data->id)}}">
+                                    <h4 class="title">{{$data->title}}</h4>
+                                </a>
+                                <p class="">
+                                    <strong>Published On:</strong>
+                                    <span>{{\Carbon\Carbon::parse($data->publish_date)->format('d M Y')}}</span>
+                                </p>
 
-                                <a href="{{asset('assets/uploads/publications/pdf/'.$data->pdf_url)}}" class="float-right text-right"  download>download</a>
+                                <a href="{{asset('assets/uploads/publications/pdf/'.$data->pdf_url)}}" target="_blank"
+                                   class="btn text-center">Download</a>
                             </div>
                         </div>
                     </div>
--- @core/resources/views/frontend/pages/publication/publication-single.blade.php
+++ @core/resources/views/frontend/pages/publication/publication-single.blade.php
@@ -18,98 +18,32 @@
 @endsection
 @section('content')
 
-    <div class="page-content service-details padding-top-120 padding-bottom-115">
+    <div class="page-content service-details common-single publication-single padding-top-50 padding-bottom-100">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <div class="service-details-item">
-                        <a href="{{asset('assets/uploads/publications/pdf/'.$service_item->pdf_url)}}" class="float-right text-right" download>download</a>
-                        <div class="thumb margin-bottom-40">
+                    <h3 class="common-single-title publication-single-title margin-bottom-15">{{$service_item->title}}</h3>
+                </div>
+                <div class="col-lg-6">
+                    <div class="service-details-item common-single-item publication-single-item position-relative">
+                        <a href="{{asset('assets/uploads/publications/pdf/'.$service_item->pdf_url)}}"
+                           class="btn">Download</a>
+                        <div class="thumb">
                             {!! render_image_markup_by_attachment_id($service_item->thumbnail) !!}
                         </div>
-                        <div class="service-description">
-                            {!! $service_item->description !!}
-                        </div>
-                        @if(!empty($price_plan))
-                        <div class="price-plan-wrapper margin-top-40">
-                            <div class="row">
-                                @foreach($price_plan as $data)
-                                <div class="col-lg-6">
-                                    <div class="single-price-plan-01 margin-bottom-20">
-                                        <div class="price-header">
-                                            <div class="name-box">
-                                                <h4 class="name">{{$data->title}}</h4>
-                                            </div>
-                                            <div class="price-wrap">
-                                                <span class="price">{{amount_with_currency_symbol($data->price)}}</span><span
-                                                        class="month">{{$data->type}}</span>
-                                            </div>
-                                        </div>
-                                        <div class="price-body">
-                                            <ul>
-                                                @php
-                                                    $features = explode("\n",$data->features);
-                                                @endphp
-                                                @foreach($features as $item)
-                                                    <li>{{$item}}</li>
-                                                @endforeach
-                                            </ul>
-                                        </div>
-                                        <div class="btn-wrapper">
-                                            @php
-                                                $url = !empty($data->url_status) ? route('frontend.plan.order',['id' => $data->id]) : $data->btn_url;
-                                            @endphp
-                                            <a href="{{$url}}" class="boxed-btn">{{$data->btn_text}}</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                    @endforeach
-                            </div>
-                        </div>
-                        @endif
                     </div>
                 </div>
-{{--                <div class="col-lg-4">--}}
-{{--                    <div class="widget-nav-menu margin-bottom-30 service-category">--}}
-{{--                        <ul>--}}
-{{--                            @foreach($service_category as $data)--}}
-{{--                                <li>--}}
-{{--                                    @php--}}
-{{--                                    $service_cat_id = !empty($service_item->category) ? $service_item->category->id : '';--}}
-{{--                                    @endphp--}}
-{{--                                    <a href="{{route('frontend.services.category', ['id' => $data->id , 'any' => Str::slug($data->name)])}}"--}}
-{{--                                       class="service-widget @if($data->id == $service_cat_id ) active @endif">--}}
-{{--                                        <div class="service-title">--}}
-{{--                                            <h6 class="title">{{$data->name}}</h6>--}}
-{{--                                        </div>--}}
-{{--                                    </a>--}}
-{{--                                </li>--}}
-{{--                            @endforeach--}}
-{{--                        </ul>--}}
-{{--                    </div>--}}
-{{--                    <div class="attorney-contact-form-wrap">--}}
-{{--                        <h3 class="title">{{get_static_option('service_single_page_' . $user_select_lang_slug . '_query_form_title')}}</h3>--}}
-{{--                        @include('backend.partials.message')--}}
-{{--                        @if($errors->any())--}}
-{{--                            <div class="alert alert-danger">--}}
-{{--                                <ul>--}}
-{{--                                    @foreach($errors->all() as $error)--}}
-{{--                                        <li>{{$error}}</li>--}}
-{{--                                    @endforeach--}}
-{{--                                </ul>--}}
-{{--                            </div>--}}
-{{--                        @endif--}}
-{{--                        <div class="attorney-contact-form">--}}
-{{--                            <form action="{{route('frontend.service.quote')}}" method="post" enctype="multipart/form-data">--}}
-{{--                                @csrf--}}
-{{--                                {!! render_form_field_for_frontend(get_static_option('service_query_form_fields')) !!}--}}
-{{--                                <div class="form-group">--}}
-{{--                                    <input type="submit" value="{{__('Submit Request')}}" class="submit-btn">--}}
-{{--                                </div>--}}
-{{--                            </form>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
+                <div class="col-lg-6">
+                    <div class="service-details-item common-single-item publication-single-item position-relative">
+                        <h5>Published On</h5>
+                        <h6 class="publish-date font-weight-bold">{{$service_item->publish_date}}</h6>
+                        <div class="service-description publication-description">
+                            {!! $service_item->description !!}
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
         </div>
     </div>
