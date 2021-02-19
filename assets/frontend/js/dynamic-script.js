fd90a92351d30ef51256911eb2951cd656dfa423
Muazzam Younus <muazzam.younus925@gmail.com>
2021-02-19 19:17:27 +0500

    Style Updates

--- assets/frontend/js/dynamic-script.js
+++ assets/frontend/js/dynamic-script.js
@@ -10,5 +10,7 @@ $(document).ready(function () {
         $(this).parents('.menu-item-has-children').addClass('active');
         $(this).toggleClass('active');
     });
-    $('footer-widget .widget-ul-wrapper ul li a').attr('target','_blank');
+
+    $('.footer-widget .widget-ul-wrapper ul li a').attr('target', '_blank');
+    
 });
