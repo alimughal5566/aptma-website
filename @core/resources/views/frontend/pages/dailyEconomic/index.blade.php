8993464ee2e7544ddb44b2bc6bc582a7e7cad53a
Muazzam Younus <muazzam.younus925@gmail.com>
2021-03-01 17:59:51 +0500

    Daily Economic Update New

--- @core/resources/views/frontend/pages/dailyEconomic/index.blade.php
+++ @core/resources/views/frontend/pages/dailyEconomic/index.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.frontend-page-master')
-@section('site-title',' Daily Economics')
+@section('site-title',' Daily Economics Updates')
 @section('page-title')
-    Daily Economics
+    Daily Economics Updates
 @endsection
 
 @section('page-meta-data')
@@ -13,31 +13,31 @@
     <section class="service-area service-page common-area circular-area circular-page padding-top-40 padding-bottom-60">
         <div class="container">
             <h2 class="font-weight-bold mb-3 text-center">
-                Daily Economics <?php echo ($category) ? "<small>($category->name)</small>" : "" ?></h2>
+                Daily Economics Updates <?php echo ($category) ? "<small>($category->name)</small>" : "" ?></h2>
 
             <div class="row">
                 @forelse($all_services as $data)
                     <div class="col-lg-4 col-md-6">
                         <div class="common-item circular-item single-what-we-cover-item-02 margin-bottom-30">
-                            <div class="common-img circular-img single-what-img position-relative">
-                                @php
-                                    $now = Carbon\Carbon::now();
-                                $datework = Carbon\Carbon::parse($data->created_at);
-                                $diff = $datework->diffInDays($now);
-                                @endphp
-                                @if($diff<15)
-                                    <small class="font-italic badge">New</small>
-                                @endif
+{{--                            <div class="common-img circular-img single-what-img position-relative">--}}
+{{--                                @php--}}
+{{--                                    $now = Carbon\Carbon::now();--}}
+{{--                                $datework = Carbon\Carbon::parse($data->created_at);--}}
+{{--                                $diff = $datework->diffInDays($now);--}}
+{{--                                @endphp--}}
+{{--                                @if($diff<15)--}}
+{{--                                    <small class="font-italic badge">New</small>--}}
+{{--                                @endif--}}
 {{--                                $pdf = \PDF::loadView('pdf.invoice', $data);--}}
 
 {{--                                <img src="{{$pdf}}"></img>--}}
 {{--                                <a href="{{route('frontend.circular.single',$data->slug)}}">{!! render_image_markup_by_attachment_id($pdf) !!}</a>--}}
-                            </div>
+{{--                            </div>--}}
                             <div class="common-content content">
                                 <a href="{{route('frontend.economic.single',$data->slug)}}">
-                                    <h4 class="title">{{$data->title}}</h4>
+                                    <h4 class="title mb-0">{{$data->title}}</h4>
                                 </a>
-                                <p>{{@$data->category->name}}</p>
+{{--                                <p>{{@$data->category->name}}</p>--}}
                                 <p>{{@$data->publish_date}}</p>
 {{--                                <a href="{{asset('assets/uploads/daily-economics/'.$data->url)}}" download target="_blank" class="btn">Download</a>--}}
                             </div>
