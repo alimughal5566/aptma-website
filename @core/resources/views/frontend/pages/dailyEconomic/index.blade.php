a71d40daeea6249b11efe0251b89f3f0c47daf75
Muazzam Younus <muazzam.younus925@gmail.com>
2021-03-02 16:52:37 +0500

    Economic Class Added

--- @core/resources/views/frontend/pages/dailyEconomic/index.blade.php
+++ @core/resources/views/frontend/pages/dailyEconomic/index.blade.php
@@ -13,11 +13,11 @@
     <section class="service-area service-page common-area economic-area economic-page padding-top-40 padding-bottom-60">
         <div class="container">
             <div class="row">
-                <div class="col-12 col-md-10">
+                <div class="col-12 col-md-9">
                     <h2 class="font-weight-bold mb-3 text-center">
                         Daily Economics Updates <?php echo ($category) ? "<small>($category->name)</small>" : "" ?></h2>
                 </div>
-                <div class="col-12 col-md-2">
+                <div class="col-12 col-md-3">
                     <div>
                         <label for="search_date" class="sr-only"></label>
                         <select class="form-control" onchange="searchRecord()" name="search_date" id="search_date">
@@ -29,8 +29,6 @@
                     </div>
                 </div>
             </div>
-
-
             <div class="row">
                 @forelse($all_services as $data)
                     <div class="col-lg-4 col-md-6">
@@ -44,21 +42,12 @@
                                 @if($diff<15)
                                     <small class="font-italic badge">New</small>
                                 @endif
-                                {{--                                $pdf = \PDF::loadView('pdf.invoice', $data);--}}
-                                {{----}}
-                                {{--                                <img src="{{$pdf}}"></img>--}}
                                 <a href="{{route('frontend.circular.single',$data->slug)}}">{!! render_image_markup_by_attachment_id($data->thumbnail) !!}</a>
                             </div>
-                            <div class="">
-                                {{--                                <a href="{{route('frontend.economic.single',$data->slug)}}">--}}
-                                {{--                                    {!! render_image_markup_by_attachment_id($data->thumbnail) !!}</a>--}}
+                            <div class="common-content content">
                                 <a href="{{route('frontend.economic.single',$data->slug)}}">
                                     <h4 class="title mb-0">{{$data->title}}</h4>
                                 </a>
-                                {{--                                <p>{{@$data->category->name}}</p>--}}
-                                <p>
-                                    <span>{{date('M d Y', strtotime(@$data->publish_date))}}</span>
-                                </p>
                                 <a href="{{asset('assets/uploads/daily-economics/'.$data->url)}}" download
                                    target="_blank" class="btn">Download</a>
                             </div>
