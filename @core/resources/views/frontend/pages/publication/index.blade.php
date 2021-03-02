b81b23cfc0e0b3b2c8d0792c9a1e72ce71664649
Muazzam Younus <muazzam.younus925@gmail.com>
2021-03-02 13:48:33 +0500

    Publication Category Title Update

--- @core/resources/views/frontend/pages/publication/index.blade.php
+++ @core/resources/views/frontend/pages/publication/index.blade.php
@@ -10,8 +10,7 @@
     <section
             class="service-area service-page common-area publication-area publication-page padding-top-40 padding-bottom-60">
         <div class="container">
-            <h2 class="font-weight-bold mb-3 text-center">
-                Publications <?php echo ($category) ? "<small>($category->name)</small>" : "" ?></h2>
+            <h2 class="font-weight-bold mb-3 text-center"><?php echo ($category) ? "($category->name)" : "" ?></h2>
 
             <div class="row">
                 @php $a = 1; @endphp
